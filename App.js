c2b8b72d9b156ce902a475f5d6af5fe500be3e6e
shiva <shivasankarvenkat1996@gmail.com>
2023-05-20 14:28:32 +0530

    Changed the primary program

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
-
 import { StyleSheet } from "react-native";
 import StartScreen from "./display/primaryScreen";
-
+export default function App() {
+  return <StartScreen />;
+}
 
 const styles = StyleSheet.create({});
-
-
