1255e62f85495013dac168e0f5d8d063d1482206
shiva <shivasankarvenkat1996@gmail.com>
2023-05-20 14:04:44 +0530

    styling primary buttons

--- Components/PrimaryButton.js
+++ Components/PrimaryButton.js
@@ -1,5 +1,5 @@
 import { View, Text } from "react-native";
-//destructured the props.childern to children and getting the values directly
+
 function PrimaryButton(props) {
   return (
     <View>
--- display/primaryScreen.js
+++ display/primaryScreen.js
@@ -1,8 +1,8 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../Components/PrimaryButton";
 function StartScreen() {
   return (
-    <View>
+    <View style={styles.input}>
       <TextInput />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
@@ -11,3 +11,16 @@ function StartScreen() {
 }
 
 export default StartScreen;
+
+const styles = StyleSheet.create({
+  input: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: "#72063c",
+    borderRadius: 15,
+    elevation: 4,
+  },
+});
