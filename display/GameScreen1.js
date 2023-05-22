83c3b0b2485151244c1e11e4551354b3e69f9837
shiva <shivasankarvenkat1996@gmail.com>
2023-05-22 22:44:53 +0530

    General Outline for game screen

--- display/GameScreen1.js
+++ display/GameScreen1.js
@@ -3,7 +3,12 @@ import { Text, View, StyleSheet } from "react-native";
 function GameScreen() {
   return (
     <View style={styles.textInputContainer}>
-      <Text>Game Screen</Text>
+      <Text>Opponent's Guess</Text>
+      Guess //PlaceHolders
+      <View>
+        <Text>Higher or Lower?</Text>+ //PlaceHolders - //PlaceHolders
+      </View>
+      <View>LOG ROUNDS</View>
     </View>
   );
 }
