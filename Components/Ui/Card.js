29398ebeef4fe3f6b59ae488fdfbb4f16dd67c92
shiva <shivasankarvenkat1996@gmail.com>
2023-05-25 09:52:43 +0530

    dynamic styles for numbercontainer comp

--- Components/Ui/Card.js
+++ Components/Ui/Card.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Dimensions } from "react-native";
 import React from "react";
 import colors from "../../Utilities/colors";
 
